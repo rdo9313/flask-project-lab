1edabd859ec3dda555121130f7c8a691220fd889
RyanD <rdo9313@gmail.com>
2022-08-27 14:44:04 -0400

    added routes for restaurants

--- src/main.py
+++
@@ -1,6 +0,0 @@
-import psycopg2
-
-conn = psycopg2.connect(database="restaurants", user="ryando")
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM restaurants LIMIT 10;")
-print(cursor.fetchall())
